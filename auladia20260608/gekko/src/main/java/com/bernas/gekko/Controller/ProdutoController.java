9297c5de09d94592d7d1bbc5737f4b4803617a94
Bernardo <bernardowilchen@gmail.com>
2026-06-08 09:30:36 -0300

    ficou pela metade. Terminar os códigos depois

---
+++ auladia20260608/gekko/src/main/java/com/bernas/gekko/Controller/ProdutoController.java
@@ -0,0 +1,10 @@
+package com.bernas.gekko.Controller;
+
+public class ProdutoController {
+    
+    @GetMapping
+
+    public String criarOrcametno(){
+        return "Orcamento criado";
+    }
+}
---
+++ auladia20260608/gekko/src/main/java/com/bernas/gekko/GekkoApplication.java
@@ -0,0 +1,13 @@
+package com.bernas.gekko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GekkoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GekkoApplication.class, args);
+	}
+
+}

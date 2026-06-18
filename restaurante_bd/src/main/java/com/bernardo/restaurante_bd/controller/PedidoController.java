e3b0f632c0b022afd0badca02a510a28522f1d33
Bernardo <bernardowilchen@gmail.com>
2026-06-18 09:20:27 -0300

    codigos da aula de 17 de junho de 2026. CRUD e conteúdos úteis para a vida!

--- restaurante_bd/src/main/java/com/bernardo/restaurante_bd/controller/PedidoController.java
+++ restaurante_bd/src/main/java/com/bernardo/restaurante_bd/controller/PedidoController.java
@@ -1,5 +1,31 @@
 package com.bernardo.restaurante_bd.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bernardo.restaurante_bd.model.Pedido;
+import com.bernardo.restaurante_bd.service.PedidoService;
+
+@RestController
+@RequestMapping("/pedidos")
 public class PedidoController {
     
+    @Autowired
+    private PedidoService pedidoService;    
+    
+    @GetMapping
+    public List<Pedido> lerPedidos(){
+        return pedidoService.lerPedidos();
+    }
+
+    @PostMapping
+    public Pedido inserirPedido(@RequestBody Pedido pedido){
+        return pedidoService.inserirPedido(pedido);
+    }
 }
--- restaurante_bd/src/main/java/com/bernardo/restaurante_bd/service/PedidoService.java
+++ restaurante_bd/src/main/java/com/bernardo/restaurante_bd/service/PedidoService.java
@@ -1,10 +1,24 @@
 package com.bernardo.restaurante_bd.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bernardo.restaurante_bd.model.Pedido;
 import com.bernardo.restaurante_bd.repository.PedidoRepository;
 
+@Service
 public class PedidoService {
+
     @Autowired
     private PedidoRepository pedidoRepository;
+
+    public List<Pedido> lerPedidos(){
+        return pedidoRepository.findAll();
+    }
+ 
+    public Pedido inserirPedido(Pedido pedido){
+        return pedidoRepository.save(pedido);
+    }
 }
